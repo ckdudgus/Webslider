2e2f1db0caecc9b32da25595a450bdf5248c0d59
ckdudgus <c91878074@gmail.com>
2022-03-15 18:20:57 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-    var px = $("#slider").width(); // 1200px
-    var dir = -1; // 방향
+    var classNm = "show" // 1200px
+    var count = 0; // 방향
+    var total = $("#slider li").length; // $("#slider li").size()
 
     //Json 포멧에서 key는 절대 변수로 사용할 수 없다
     // "marginLeft" ->key , px*dir -> 값 
     setInterval(function(){
-        $("#slider ul").animate( { "marginLeft" : px*dir } , 400 , function(){
-            $(this).find("li").eq(0).appendTo($(this));
-            $(this).css("margin-left" , 0); 
-        });        
-    }, 3000);
+        count++
+        count %= total; // 0~2
+        // $("#slider li").removeClass(classNm);
+        $("#slider li").eq(count).addClass(classNm).siblings().removeClass(classNm)
+    } , 3000);
+        
 })
